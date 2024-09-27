7cd2e3c0d48a0b8ef7b5cbc7376d553ca8e0e95a
Sergei Remarenko <sergremarenko@gmail.com>
2024-09-27 15:19:26 +0300

    add pages with lazy chunks

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -5,5 +5,6 @@ export function buildDevServer(options: BuildOptions): DevServerConfiguration {
     return {
         port: options.port ?? 6783,
         open: true,
+        historyApiFallback: true,
     }
 }
--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import styles from "./index.module.scss";
+import { Link, Outlet } from "react-router-dom";
 
 export function App() {
   const [counter, setCounter] = useState<number>(0);
@@ -8,10 +9,14 @@ export function App() {
 
   return (
     <div>
+      <Link to={"/about"}>about</Link>
+      <br/>
+      <Link to={"/shop"}>shop</Link>
       <h1>{counter}</h1>
       <button className={styles.button} onClick={increment}>
         <span>increment</span>
       </button>
+      <Outlet />
     </div>
   );
 }
