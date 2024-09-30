ecb73c3b8e4ac4f0812223e59d79a53e322952c7
Sergei Remarenko <sergremarenko@gmail.com>
2024-09-30 18:51:17 +0300

    try babel

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,6 +1,6 @@
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import ReactRefreshTypeScript from "react-refresh-typescript";
-import { ModuleOptions } from "webpack";
+import { ModuleOptions, runtime } from "webpack";
 import { BuildOptions } from "./types/types";
 
 export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
@@ -67,10 +67,28 @@ export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
         ]
     }
 
+    const babelLoader = {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: [
+                    '@babel/preset-env',
+                    '@babel/preset-typescript',
+                    ['@babel/preset-react', {
+                        runtime: isDev ? "automatic" : "classic"
+                    }],
+                ]
+            }
+        }
+    }
+
     return [
         assetLoader,
         scssLoader,
-        tsLoader,
+        // tsLoader,
+        babelLoader,
         svgReactLoader
     ]
 }
